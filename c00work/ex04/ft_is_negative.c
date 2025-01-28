feb8fefe1bb90a42af2241be0cb357775d32651b
Lukia Gotoh <lgotoh@F2N066.42adel.org.au>
2025-01-28 15:28:38 +1030

    formatting

--- c00work/ex04/ft_is_negative.c
+++ c00work/ex04/ft_is_negative.c
@@ -6,28 +6,27 @@
 /*   By: lgotoh <lgotoh@student.42adel.org.au>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 13:42:37 by lgotoh            #+#    #+#             */
-/*   Updated: 2025/01/28 14:22:56 by lgotoh           ###   ########.fr       */
+/*   Updated: 2025/01/28 15:26:36 by lgotoh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_is_negative(int n);
+void	ft_is_negative(int n);
 
-int main()
-{
-    char x;
-    int n = 1;
+int	main(void)
+{	
+	char	x;
+	int		n;
 
-    if(n < 0)
-    {
-	x = 'N';
-    }
-
-    else
-    {
-	x = 'P';
-    }
-
-    write(1, &x, 1);
+	n = 1;
+	if (n < 0)
+	{
+		x = 'N';
+	}
+	else
+	{
+		x = 'P';
+	}
+	write(1, &x, 1);
 }
