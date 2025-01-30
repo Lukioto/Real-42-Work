36174d86a41cd8f2806c2ab8668839d5f6801164
Lukia Gotoh <lgotoh@F2N066.42adel.org.au>
2025-01-30 15:00:51 +1030

    working

---
+++ c01work/ex01/ft_ultimate_ft.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgotoh <lgotoh@student.42adel.org.au>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 13:04:35 by lgotoh            #+#    #+#             */
+/*   Updated: 2025/01/30 13:46:48 by lgotoh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+/*int	main(void)
+{
+	int	num = 0;
+	int	*ptr1 = &num;
+	int	**ptr2 = &ptr1;
+	int	***ptr3 = &ptr2;
+	int	****ptr4 = &ptr3;
+	int	*****ptr5 = &ptr4;
+	int	******ptr6 = &ptr5;
+	int	*******ptr7 = &ptr6;
+	int	********ptr8 = &ptr7;
+	int	*********ptr9 = &ptr8;
+	printf("%d", num);
+	ft_ultimate_ft(ptr9);
+	printf("\n%d\n", num);
+	return(0);
+}*/
--- c01work/ex02/ft_swap.c
+++ c01work/ex02/ft_swap.c
@@ -1,28 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lgotoh <lgotoh@student.42adel.org.au>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/29 16:05:51 by lgotoh            #+#    #+#             */
-/*   Updated: 2025/01/30 13:16:34 by lgotoh           ###   ########.fr       */
+/*   Created: 2025/01/30 13:56:22 by lgotoh            #+#    #+#             */
+/*   Updated: 2025/01/30 14:28:10 by lgotoh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-void	ft_ft(int *nbr)
+void	ft_swap(int *a, int *b)
 {
-	*nbr = 42;
-}
+	int temp = *a;
+	a* = b*;
+	b* = temp;
+}	
 
-/*int main(void)
-{
-	int	num = 0;
-	int	*ptr = &num;
-	printf("%d", num);
-	ft_ft(ptr);
-	printf("\n%d\n", num);
-	return(0);
-}*/
+int main(void)
+{	
+	int	num1 = 10;
+	int	num2 = 20;
+	int	*ptr1 = &num1;
+	int	*ptr2 = &num2;
+	printf("%d", num1);
+	printf("\n%d\n", num2);
+	ft_swap(*ptr1, *ptr2);
+	printf("%d", num1);
+	printf("\n%d\n", num2);
+	return (0);
+}
