a072ef624463a493223b005d39c77c2c61abb7c5
Lukia Gotoh <lgotoh@F2N066.42adel.org.au>
2025-01-30 15:26:18 +1030

    finished swap started div mod

--- c01work/ex03/ft_div_mod.c
+++ c01work/ex03/ft_div_mod.c
@@ -1,36 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lgotoh <lgotoh@student.42adel.org.au>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/30 13:56:22 by lgotoh            #+#    #+#             */
-/*   Updated: 2025/01/30 15:10:44 by lgotoh           ###   ########.fr       */
+/*   Created: 2025/01/30 15:22:44 by lgotoh            #+#    #+#             */
+/*   Updated: 2025/01/30 15:25:01 by lgotoh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+void	ft_div_mod(int a, int b, int *div, int *mod);
 
-void	ft_swap(int *a, int *b)
+int main()
 {
-	int	temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}	
-
-/*int main(void)
-{	
-	int	num1 = 10;
-	int	num2 = 20;
-	int	*ptr1 = &num1;
-	int	*ptr2 = &num2;
-	printf("%d", num1);
-	printf("\n%d\n", num2);
-	ft_swap(ptr1, ptr2);
-	printf("%d", num1);
-	printf("\n%d\n", num2);
-	return (0);
-}*/
+	
+}
