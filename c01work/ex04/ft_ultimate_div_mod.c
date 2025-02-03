dff1a38e6c4ae5a170e0525972d10213602f0242
Lukia Gotoh <lgotoh@F2N064.42adel.org.au>
2025-02-03 12:15:11 +1030

    finished ex03, added ex04 ls

--- c01work/ex04/ft_ultimate_div_mod.c
+++ c01work/ex04/ft_ultimate_div_mod.c
@@ -1,30 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lgotoh <lgotoh@student.42adel.org.au>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/30 15:22:44 by lgotoh            #+#    #+#             */
-/*   Updated: 2025/02/03 11:21:06 by lgotoh           ###   ########.fr       */
+/*   Created: 2025/02/03 11:23:10 by lgotoh            #+#    #+#             */
+/*   Updated: 2025/02/03 12:12:15 by lgotoh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-void	ft_div_mod(int a, int b, int *div, int *mod)
+void	ft_ultimate_div_mod(int *a, int *b)
 {
-	*div = a / b;
-	*mod = a % b;
+	int	num1;
+	int	num2;
+
+	num1 = *a;
+	num2 = *b;
+	*a = num1 / num2;
+	*b = num1 % num2;
 }
 
 /*int main()
 {
-	int	a = 23;
-	int	b = 5;
-	int	div;
-	int	mod;
+	int	a = 56;
+	int	b = 10;
+	int	olda = a;
+	int	oldb = b;
 
-	ft_div_mod(a, b, &div, &mod);
-	printf("%d, %d, %d, %d", a, b, div, mod);
+	ft_ultimate_div_mod(&a, &b);
+	printf("%d, %d, %d, %d", olda, oldb, a, b);
 }*/
