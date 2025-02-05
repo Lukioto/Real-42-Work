45a5df0b2b96866e434a6d85584f58bc27fa570b
Lukia Gotoh <lgotoh@F2N064.42adel.org.au>
2025-02-05 14:15:14 +1030

    work

--- c02work/ex01/ft_strncpy.c
+++ c02work/ex01/ft_strncpy.c
@@ -1,35 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lgotoh <lgotoh@student.42adel.org.au>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/03 15:35:14 by lgotoh            #+#    #+#             */
-/*   Updated: 2025/02/05 14:11:36 by lgotoh           ###   ########.fr       */
+/*   Created: 2025/02/05 11:41:03 by lgotoh            #+#    #+#             */
+/*   Updated: 2025/02/05 14:08:22 by lgotoh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <string.h>
 
-char	*ft_strcpy(char	*dest, char *src)
+char	*ft_strcpy(char	*dest, char *src, unsigned int n)
 {
-	while (*src != '\0')
+	int	count;
+
+	count = 0;
+	while (*src != '\0' && count < n)
 	{
 		*dest = *src;
 		*dest++;
 		*src++;
+		count++;
 	}
 }
 
 /*int main()
 {
 	char text1[] = "This will be replaced";
-	char text2[] = "This will be copied";
-	
+	char text2[] = "12345678- Only first 8 characters will copy";
+	unsigned int charlimit = 8;
+
 	printf("%s", text1);
 	printf("\n%s\n", text2);
-	ft_strcpy(text1, text2);
+	ft_strcpy(text1, text2, charlimit);
 	printf("\n%s", text1);
 	printf("\n%s", text2);
 }*/
