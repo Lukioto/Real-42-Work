f1327571201373bce0c31f876abe3710acae30cd
Lukia Gotoh <lgotoh@F2N010.42adel.org.au>
2025-02-10 15:05:13 +1030

    finishing c02

---
+++ c02work/ex04/ft_str_is_lowercase.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgotoh <lgotoh@student.42adel.org.au>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 15:59:26 by lgotoh            #+#    #+#             */
+/*   Updated: 2025/02/06 16:11:47 by lgotoh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_str_is_lowercase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] < 'a' || str[i] > 'z')
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+/*int main()
+{
+	printf("%d", ft_str_is_lowercase("thisislowercase"));
+	printf("\n%d", ft_str_is_lowercase("THISISUPPERCASE"));
+	printf("\n%d", ft_str_is_lowercase(""));
+
+}*/
---
+++ c02work/ex05/ft_str_is_uppercase.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgotoh <lgotoh@student.42adel.org.au>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 16:17:12 by lgotoh            #+#    #+#             */
+/*   Updated: 2025/02/06 16:21:48 by lgotoh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_str_is_uppercase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] < 'A' || str[i] > 'Z')
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+/*int main()
+{
+	printf("%d", ft_str_is_uppercase("thisislowercase"));
+	printf("\n%d", ft_str_is_uppercase("THISISUPPERCASE"));
+	printf("\n%d", ft_str_is_uppercase(""));
+
+}*/
